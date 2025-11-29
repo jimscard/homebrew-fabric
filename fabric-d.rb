e70f6e5e96e6708a0eace6e99bfd0b854590677a
Jim Scardelis <jim@jceltd.com>
2025-11-29 02:25:18 -0800

    Add initial formula for Fabric-d command-line tools

---
+++ fabric-d.rb
@@ -0,0 +1,20 @@
+class FabricD < Formula
+    desc "Fabric-d command-line tools"
+    homepage "https://github.com/jimscard/fabric-d"
+    url "https://github.com/jimscard/fabric-d/archive/refs/heads/main.zip"
+    sha256 "TODO" # Run: brew fetch --HEAD fabric-d to get the sha256
+
+    def install
+        # Install all .sh files from the repository
+        Dir.glob("*.sh").each do |sh_file|
+            # Get basename without extension
+            cmd_name = File.basename(sh_file, ".sh")
+            # Install executable in bin folder
+            bin.install sh_file, cmd_name
+        end
+    end
+
+    test do
+        system "#{bin}/your-command-name", "--help"
+    end
+end
