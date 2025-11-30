71eb990175993a86d0d1959fafc84d06419d47a4
Jim Scardelis <jim@jceltd.com>
2025-11-29 19:57:34 -0800

    Format code for consistency in FabricD formula

--- fabric-d.rb
+++ fabric-d.rb
@@ -1,30 +1,30 @@
 class FabricD < Formula
-    desc "Fabric-d command-line tools"
-    homepage "https://github.com/jimscard/fabric-d"
-    url "https://github.com/jimscard/fabric-d/archive/refs/tags/v1.0.tar.gz"
-    sha256 "0297f2de29810da185e365a92a22d63eb797707003ae883ce772a1a56a8f2126"
-    license "MIT"
-    head "https://github.com/jimscard/fabric-d.git", branch: "main"
-    version "1.0"
+  desc "Fabric-d command-line tools"
+  homepage "https://github.com/jimscard/fabric-d"
+  url "https://github.com/jimscard/fabric-d/archive/refs/tags/v1.0.tar.gz"
+  sha256 "0297f2de29810da185e365a92a22d63eb797707003ae883ce772a1a56a8f2126"
+  license "MIT"
+  head "https://github.com/jimscard/fabric-d.git", branch: "main"
+  version "1.0"
 
-    livecheck do
-        url :stable
-        strategy :github_latest
-    end
+  livecheck do
+    url :stable
+    strategy :github_latest
+  end
 
-    def install
-        # Install all .sh files from the repository
-        Dir.glob("*.sh").each do |sh_file|
-            # Get basename without extension
-            cmd_name = File.basename(sh_file, ".sh")
-            # Make the script executable
-            chmod 0755, sh_file
-            # Install executable in bin folder
-            bin.install sh_file, cmd_name
-        end
+  def install
+    # Install all .sh files from the repository
+    Dir.glob("*.sh").each do |sh_file|
+      # Get basename without extension
+      cmd_name = File.basename(sh_file, ".sh")
+      # Make the script executable
+      chmod 0755, sh_file
+      # Install executable in bin folder
+      bin.install sh_file => cmd_name
     end
+  end
 
-    test do
-        system "#{bin}/fabric-d", "--help"
-    end
+  test do
+    system "#{bin}/fabric-d", "--help"
+  end
 end
