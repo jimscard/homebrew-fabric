db57609a6de4f25bb6a188efebb3b70145f38c67
Jim Scardelis <jim@jceltd.com>
2025-11-29 19:50:29 -0800

    Make script executable before installation in FabricD formula

--- fabric-d.rb
+++ fabric-d.rb
@@ -17,6 +17,8 @@ class FabricD < Formula
         Dir.glob("*.sh").each do |sh_file|
             # Get basename without extension
             cmd_name = File.basename(sh_file, ".sh")
+            # Make the script executable
+            chmod 0755, sh_file
             # Install executable in bin folder
             bin.install sh_file, cmd_name
         end
